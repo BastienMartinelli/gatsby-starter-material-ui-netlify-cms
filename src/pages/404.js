7c2e6816ddf41e405668ddbb6fb97fcf6602c25d
Bastien <bastien@pop-os.localdomain>
2019-08-25 15:18:05 +0200

    clean + 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import Layout from "../components/Layout";
+import { Container, Grid, Typography, Paper } from "@material-ui/core";
+import makeStyles from "@material-ui/styles/makeStyles";
 
-const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
-)
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: 20
+  },
+  title: {
+    marginBottom: 24
+  }
+}));
 
-export default NotFoundPage
+const NotFoundPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Layout>
+      <Container maxWidth="sm">
+        <Grid container justify="center">
+          <Paper className={classes.paper}>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              className={classes.title}
+            >
+              404
+            </Typography>
+            <Typography color="textSecondary" variant="h5" align="center">
+              You just hit a route that doesn&#39;t exist... the sadness.
+            </Typography>
+          </Paper>
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
