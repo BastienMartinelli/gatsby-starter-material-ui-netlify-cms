d223c4eaa2d47a2552bf2e5bfc4af1075f33d866
Bastien <bastien@pop-os.localdomain>
2019-08-25 17:05:37 +0200

    add theme provider to template

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import React from "react";
+import PropTypes from "prop-types";
+import { BlogPostTemplate } from "../../templates/blog-post";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../../theme";
 
 const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-)
+  <ThemeProvider theme={theme}>
+    <BlogPostTemplate
+      content={widgetFor("body")}
+      description={entry.getIn(["data", "description"])}
+      tags={entry.getIn(["data", "tags"])}
+      title={entry.getIn(["data", "title"])}
+    />
+  </ThemeProvider>
+);
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
-    getIn: PropTypes.func,
+    getIn: PropTypes.func
   }),
-  widgetFor: PropTypes.func,
-}
+  widgetFor: PropTypes.func
+};
 
-export default BlogPostPreview
+export default BlogPostPreview;
